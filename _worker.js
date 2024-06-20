a175e36df682560db9ed7a3535925b7820a8dd1c
Kmaungmg <hankoko21@icloud.com>
2024-06-20 14:49:45 +0630

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,7 +3,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = '2a009acc-a28f-4578-9876-b09dd31790be';
+let userID = '8f1077f8-f7dc-4cd8-8074-e43576d7fc5c';
 
 const พร็อกซีไอพีs = ['cdn.xn--b6gac.eu.org', 'cdn-all.xn--b6gac.eu.org', 'workers.cloudflare.cyou'];
 
